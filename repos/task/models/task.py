211a76c9fa6be7a3c488588eb0d4667150f2b170
Oscar Alvarado <70240884+oscarjiram@users.noreply.github.com>
2023-04-04 21:58:33 +0000

    Update task.py

--- repos/task/models/task.py
+++ repos/task/models/task.py
@@ -1,10 +1,34 @@
 from odoo import models, fields
 
+
 class Task(models.Model):
-    _name = 'task'
+    _name = 'tarea.prueba'
     _description = 'Tarea'
 
     name = fields.Char('Nombre', required=True)
     description = fields.Text('Descripción')
     deadline = fields.Date('Fecha límite')
     completed = fields.Boolean('Completado', default=False)
+    duration = fields.Integer(string='Duración de la tarea (min)')
+    user_id = fields.Many2one('res.users', string='Asignado a')
+    state = fields.Selection([
+        ('pendiente', 'Pendiente'),
+        ('progreso', 'En Progreso'),
+        ('pausada', 'Pausada'),
+        ('finalizada', 'Finalizada'),
+    ], default='pendiente')
+
+    def action_iniciar(self):
+        self.write({'state': 'progreso'})
+
+    def action_pausar(self):
+        self.write({'state': 'pausada'})
+
+    def action_finalizar(self):
+        self.write({'state': 'finalizada'})
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    task_ids = fields.One2many('tarea.prueba', 'user_id', string='Tareas')
