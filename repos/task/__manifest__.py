cebe933ab1ea14325128940bb3c198371027adea
Oscar Alvarado <70240884+oscarjiram@users.noreply.github.com>
2023-04-10 22:46:48 +0000

    Update __manifest__.py

--- repos/task/__manifest__.py
+++ repos/task/__manifest__.py
@@ -2,9 +2,11 @@
     'name': 'Módulo de tareas',
     'version': '1.0',
     'description': 'Un módulo básico de Odoo para administrar una lista de tareas.',
-    'depends': ['base'],
+    'depends': ['base', 'calendar'],
     'data': [
-        'views/task_views.xml'
-    ],
+        'views/task_views.xml',
+        'security/groups.xml',
+        'security/ir.model.access.csv'],
+
 }
 
